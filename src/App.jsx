14522b1a40a14bc3156c933287925c545b4678e2
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-07-09 00:00:00 -0700

    Get saved places from localStorage and set them as the default value for the pickedPlaces State.

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
 function App() {
+  const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find(place => place.id === id));
+
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
